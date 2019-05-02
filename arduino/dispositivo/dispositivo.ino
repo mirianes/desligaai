cb404d0e62e32a2d8621f3a4b2348258a68a7d21
Douglast1997 <dtrps@cin.ufpe.br>
2019-05-02 15:45:28 -0300

    Ligar e desligar relé utilizando protocolo mqtt.

--- arduino/dispositivo/dispositivo.ino
+++ arduino/dispositivo/dispositivo.ino
@@ -3,6 +3,7 @@
 #include <PubSubClient.h> 
 #include <ArduinoJson.h> // https://github.com/bblanchon/ArduinoJson/releases/tag/v5.0.7
 
+
 //atualize SSID e senha WiFi
 const char* ssid = "IBMHackatruckIoT";
 const char* password = "IOT2017IBM";
@@ -16,7 +17,7 @@ int pino_sct = A0;                // Pino do sensor SCT entrada (analógica) do 
 int porta_rele = D1;              // Controle do rele
 
 //Atualize os valores de Org, device type, device id e token
-#define ORG "mp7emo"
+#define ORG "mf1d9p"
 #define DEVICE_TYPE "nodemcu"
 #define DEVICE_ID "nodemcu09"
 #define TOKEN "12345678"
@@ -25,6 +26,7 @@ int porta_rele = D1;              // Controle do rele
 //broker messagesight server
 char server[] = ORG ".messaging.internetofthings.ibmcloud.com";
 char topic[] = "iot-2/evt/status/fmt/json";
+const char cmdTopic[] = "iot-2/cmd/rele/fmt/json";
 char authMethod[] = "use-token-auth";
 char token[] = TOKEN;
 char clientId[] = "d:" ORG ":" DEVICE_TYPE ":" DEVICE_ID;
@@ -35,7 +37,7 @@ char IrmsString[6];
 float potencia = 0.0;
 char potenciaString[6];
 bool statusRele = false;
-String statusDispositivo;
+String statusDispositivo = "1";
 
 
 WiFiClient wifiClient;
@@ -51,11 +53,14 @@ void callback(char* topic, byte* payload, unsigned int payloadLength) {
 
   // Switch on the LED if an 1 was received as first character
   if (payload[0] == '1') {
-    digitalWrite(BUILTIN_LED, HIGH);   // Turn the LED on (Note that LOW is the voltage level
+    digitalWrite(porta_rele, HIGH);   // Turn the LED on (Note that LOW is the voltage level
     // but actually the LED is on; this is because
     // it is acive low on the ESP-01)
+    statusDispositivo = "0";
+    
   } else {
-    digitalWrite(BUILTIN_LED, LOW);  // Turn the LED off by making the voltage HIGH
+    digitalWrite(porta_rele, LOW);  // Turn the LED off by making the voltage HIGH
+    statusDispositivo = "1";
   }
   Serial.println("CALLBACK");
 }
@@ -83,6 +88,8 @@ void setup() {
   emon1.current(pino_sct, 29);
   pinMode(LED_BUILTIN, OUTPUT);
   pinMode(porta_rele, OUTPUT);
+
+  mqttConnect();
 }
 
 void serialPrintResults(String payload, int length) {
@@ -117,11 +124,37 @@ void mqttConnect() {
       Serial.print(".");
       delay(500);
     }
-    Serial.println();
+    if (client.subscribe(cmdTopic)) {
+      Serial.println("subscribe to responses OK");
+    } else {
+      Serial.println("subscribe to responses FAILED");
+    }
   }
 }
 
-void prepareData() {
+
+void controlPower() {
+  // Informa para o usuário se o dispositivo está ligado ou desligado
+  if(statusRele) 
+  {
+    statusDispositivo = "1";
+    statusRele = false;
+    digitalWrite(porta_rele, LOW); //Desliga rele
+    delay(1000);
+   
+  } 
+  
+  else 
+  {
+    statusDispositivo = "0";
+    statusRele = true;
+    digitalWrite(porta_rele, HIGH); //Liga rele
+    delay(1000);
+  }
+}
+
+void publishData() {
+
     //Calcula a corrente  
   Irms = emon1.calcIrms(1480);
   potencia = Irms * rede;
@@ -134,30 +167,6 @@ void prepareData() {
   String IrmsString =  String(TempString);
   dtostrf(potencia, 2, 1, TempString);
   String potenciaString = String(TempString);
-}
-
-void controlPower() {
-  // Informa para o usuário se o dispositivo está ligado ou desligado
-  if(statusRele) 
-  {
-    statusDispositivo = "Ligado";
-    statusRele = false;
-    digitalWrite(porta_rele, LOW); //Desliga rele
-    delay(1000);
-   
-  } 
-  
-  else 
-  {
-    statusDispositivo = "Desligado";
-    statusRele = true;
-    digitalWrite(porta_rele, HIGH); //Liga rele
-    delay(1000);
-  }
-}
-
-void publishData() {
-
   // Prepara JSON para IOT Platform
   int length = 0;
 
@@ -181,11 +190,12 @@ void publishData() {
 }
 
 void loop() {
-  mqttConnect();
-  prepareData();
-  controlPower();
+  if(!client.loop()) {
+    mqttConnect();  
+  }
+  //controlPower();
   publishData();
-  client.loop();
+  
   
   /*Função para receber informação do app
    * getUserInfo(localization, status)
